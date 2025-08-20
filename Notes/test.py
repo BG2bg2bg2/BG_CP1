ba9be820d564e6259e4399dab60a42340dae9179
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-08-20 09:46:44 -0600

    Hi1

--- Notes/test.py
+++ Notes/test.py
@@ -3,3 +3,8 @@ print("Update")
 print("Hi")
 print("LO")
 #print() <= Everything inside of the parentheses is going to be printed
+hi = input("What is your name?")
+print(hi)
+print("have a great day "+ hi + "!!!! bye >:)")
+print("good day")
+
---
+++ Practices.py/hello_world.py
@@ -0,0 +1,18 @@
+# BG 1st  Inputs and Ouputs Notes
+
+#5 what is an input
+#A way for the code to get information
+
+#6 what is the purpose of inputs
+#Code isn't helpful if it doesn't use new information
+
+# name = input("What is your name")
+# ^  ^  ^            ^
+#Save the input where it can be accessed again
+# Use keyword "Input"
+#Inside parens give user instructions
+
+print("waht is our name?")
+name = input()
+#Both instruction methoods work 
+print("Hello",name)
