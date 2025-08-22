49c01a947e122556049b9b26455b758bff466b6c
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-08-22 09:12:27 -0600

    this is done

---
+++ starters.py/variable_code.py
@@ -0,0 +1,12 @@
+#BG, variable_code
+#num1, num2,num3, num4
+#they are a way to something similer while having integers and floats mixed in
+num1 = 5
+num2 = 7
+result = num1 + num2
+print(result)
+
+num3 = 3.5
+num4 = 2.8
+result = num3 / num4 
+print(result)
