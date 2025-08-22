ea311da6e75c44a4e56a01161e13123cbfd68f84
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-08-22 09:52:37 -0600

    hi this sucks

---
+++ Practices.py/who_are_you.py
@@ -0,0 +1,16 @@
+#BG, 1st, Who are you
+#name = input("What is your name?")
+#print(f"hi" + "{name}")
+#age = input("How old are you?")
+#print(age)
+#color = input("What is your favorite color?")
+#print(color)
+#print(name + age + color)
+
+go = True
+names = []
+while go == True:
+        if names == input("What is your name"):
+            print(f"Hi" + "{name}")
+        age = input("How old are you?")
+        print(f"{age}")
