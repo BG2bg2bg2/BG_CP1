71a58c0ceed06e132e724f6c03be9bcb6788400c
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-08-25 09:15:25 -0600

    dfsf

---
+++ projects.py/who_are_you.py
@@ -0,0 +1,8 @@
+#BG, 1st, Who are you
+hi = input("What is your name?")
+print(hi)
+a = input("May I ask how old you are?")
+print(a)
+C = input("What is your favorite color?")
+print(C)
+print(f"Hi {hi} What do you like about your {a}? Why is {C} your favorite color?")
---
+++ value.py
@@ -0,0 +1,9 @@
+#BG 1st, value
+number = 5
+print("step 1 - number is", number)
+
+number = 10
+print("step 2 - number is", number)
+
+number = 0
+print("step 3 - number is", number)
