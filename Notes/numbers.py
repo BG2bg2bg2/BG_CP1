e91af13c7043b9c2072dc6f22752829c01662fb7
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-08-27 09:53:29 -0600

    numbers

---
+++ Notes/numbers.py
@@ -0,0 +1,37 @@
+# BG 1st intergers and floats notes
+
+# interger
+
+name = 10323213
+social_scecutity_number = 23423423424
+motivation =-5
+print(name, social_scecutity_number, motivation)
+
+#float (floating point number)
+pi = 3.14
+percent= -.75
+damage = 9.75
+
+#math
+print(5+2) #7
+print(5-3)#3
+print(5*2)#10
+print(5/2)#2.5
+print(5//2)#2
+print(5**2)#25
+
+print(5+2 *9 + (5**5) %3)
+
+#type casting
+num = int(3.14) #does not round just gets rid of the float.
+print(num)
+
+# using variables
+hours = 3
+minutes = 4
+
+pint(hours*60 + minutes)
+
+age = input("How old are you \n")
+
+print("Wow you are old that is", 3453453*age, "minutes" )
