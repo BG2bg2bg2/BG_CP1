00257c3a5d706c42ef6409d1d71f2a027c40fa9d
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-09-02 09:54:34 -0600

    dsafdsfdf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

---
+++ Notes/strings.py
@@ -0,0 +1,51 @@
+# BG 1st Strings Notes
+
+#What is a string?
+# a coletion of chariters
+
+#string examples
+sentence = "The quick brown fox jumps ovetr the lazy dog!"
+
+first_name = "Ty"
+month = "Sempember"
+book = "The Return of the King"
+food = "Chocolate"
+hi = "The bad dog"
+print (len(hi))
+print(month)
+
+
+#Why are they called strings?
+#
+
+#How do I make a string?
+sentince = "Content of the string"
+print(sentince)
+
+#Why do we use strings?
+
+
+#Does it matter if I use single or double quotes?
+
+
+#How do we get the length of our string?
+
+
+#What is an escape character?
+print("\t\"inkhaert\" is the best book \never not")
+
+#What does it mean to concatenate a string?
+hi = "hi name what do you like"
+bye = "bye"
+print(hi + " " + bye)
+
+#How do you concatenate a string?
+
+
+#How do I slice a string?
+
+
+#What is index?
+hie = int(input (hi[]))
+hillo = hie
+print (hillo [])
