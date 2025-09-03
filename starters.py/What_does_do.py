905582785e362f4fdb71aa1af76ac816b19c6c93
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-09-03 09:26:22 -0600

    h8i
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

---
+++ starters.py/What_does_do.py
@@ -0,0 +1,8 @@
+# BG 1st What does this code do?
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+full_name = first_name + " " + last_name
+message = "Hello, " + full_name + "! You are " + age + " Years old."
+print(message)
