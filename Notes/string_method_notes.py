c2939ba40c2a7d5a5c46444106cc0327bb651a30
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-09-04 09:27:59 -0600

    adsf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

---
+++ Notes/string_method_notes.py
@@ -0,0 +1,11 @@
+#BG 1st string method notes
+
+subject = "computer proramming 1!"
+
+print(subject.upper())
+print(subject)
+
+#stupid/idiot proofing inputs
+color = input("what is your fvorite color?").strip()
+
+print(" that is realy nice " + color + " Wow")
