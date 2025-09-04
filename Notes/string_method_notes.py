d69de65bf690d50a687da61f3b730b5a17571902
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-09-04 09:53:05 -0600

    afdasdf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- Notes/string_method_notes.py
+++ Notes/string_method_notes.py
@@ -6,6 +6,6 @@ print(subject.upper())
 print(subject)
 
 #stupid/idiot proofing inputs
-color = input("what is your fvorite color?").strip()
-
-print(" that is realy nice " + color + " Wow")
+color = input("what is your fvorite color?").strip().lower()
+by = input("Lie")
+print(" that is realy {by} hi nice {color} Wow".format(color = color, by = by))
