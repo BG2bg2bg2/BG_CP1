6694c008121ad491c7a949afd9bcddf5354c01b2
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-09-08 09:53:51 -0600

    sadfsa
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- projects.py/basic_calculator.py
+++ projects.py/basic_calculator.py
@@ -1,8 +1,28 @@
 # BG 1st Basic calucator
 
-n = False
-while n == False:
 
+def is_n(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
+  
+
+while True:
     n1 = input("enter a number")
+    if is_n(n1) == False:
+       print("Must be a number")
+    else:
+       break
+while True:
     n2 = input("enter a second number")
+    if is_n(n2) == False:
+       print("must be a number")
+    else:
+       break
 
+while True:
+    n1 = int(n1)
+    n2 = int(n2)
+    print(n1 {} n2)
