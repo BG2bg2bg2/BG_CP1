169c8c131855a47578f06c3730cadbf70ef8d8ee
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-09-08 09:34:29 -0600

    ghgh
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- starters.py/formatting_notes.py
+++ starters.py/formatting_notes.py
@@ -1,1 +1,33 @@
 #BG 1st formatting output notes
+#How do I write the format method?
+Name = "Jake"
+Age = 999
+money = 25765765765.23
+percent = .43
+print("Hello {}, your are {:E}. That is so old! You have ${:.2f} you are broke Random personent {:%}".format(Name, Age,money, percent))
+
+
+#What does the format method allow me to change about my outputs?
+
+
+
+#Describe why it is important to format your outputs
+
+
+
+
+#What is an f-string?
+
+
+
+#How do I create an f-string?
+
+
+
+#What do f-strings allow me to do?
+
+
+
+print("Hello {}, your are {:E}. That is so old! You have ${:.2f} you are broke Random personent {:%}".format(Name, Age,money, percent))
+
+print(f"\n\nHello {Name}, you are {Age:,}, That is so old. you have ${money:2f} you are broke {percent:.2f}%. ")
