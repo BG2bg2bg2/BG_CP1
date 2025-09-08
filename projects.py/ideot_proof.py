c4521b7e0b300b6e5377d77e4c5895b17b7987bd
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-09-07 21:22:56 -0600

    Update ideot_proof.py

--- projects.py/ideot_proof.py
+++ projects.py/ideot_proof.py
@@ -1,23 +1,67 @@
 #BG 1st ideot proofing
+def is_number(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
 
-fname = input("enter your first name")
-if len(fname) > 47:
-    print("your name can't be over 47 leters")
-elif not fname.find(" ") == -1:
+while True:
+  fname = input("enter your first name ")
+  if len(fname) > 15:
+     print("your name can't be over 15 letters")
+  elif not fname.find(" ") == -1:
     print("no spaces allowed for first name")
-elif not fname.isalpha():
-    print("Your name cant have numbers")
-else:
-    lname = input("enter your last name")
-    if len(lname) > 746:
-        print("last name is too long and only one man had it that long ever recoded")
-    elif not lname.find(" ") == -1:
-        print("must not have spaces")
-    elif not lname.isalpha():
-        print("last name must not have have numbers")
-    else:
-        gpa = input("enter your gpa")
-        if len(gpa) > 4:
-            print("must if 5.0 is gpa then use 4")
-        else:
-            print(fname)
+  elif not fname.isalpha():
+      print("You must enter a name and it cant have numbers")
+  else:
+    break
+
+while True:
+  lname = input("enter your last name ")
+  if len(lname) > 20:
+     print("last name is too long")
+  elif not lname.find(" ") == -1:
+     print("must not have spaces")
+  elif not lname.isalpha():
+     print("last name must not have have numbers ")
+  else:
+    break
+
+while True:
+     gpa = input("what is your gpa? ")
+     if is_number(gpa) == False:
+        print("Gpa must be a number")
+     elif float(gpa) > 4.0:
+        print("Must be 4.0 or smaller")
+     elif float(gpa) < 0:
+        print("needs to be bigger than 0")
+     else:
+      break
+
+while True:
+      pnumb1 = input("What is the first 3 numbers of your phone number? ")
+      if is_number(pnumb1) == False or len(pnumb1) != 3:
+        print("enter exactly 3 digits")
+      else:
+        break
+
+while True:
+      pnumb2 = input("What is the next 3 numbers of your phone number? ")
+      if is_number(pnumb2) == False or len(pnumb2) != 3:
+        print("enter a number with exactly 3 digits")
+      else:
+        break
+while True:
+      pnumb3 = input("What is the last 4 numbers of your phone number? ")
+      if is_number(pnumb3) == False or len(pnumb3) != 4:
+        print(f"Please enter a number with exactly four digits.")
+      else:
+        break
+
+fn = fname.capitalize()
+ln = lname.capitalize()
+print(f"\n \nname: {fn} {ln}")
+gr = float(gpa)
+p = (f"{pnumb1} {pnumb2} {pnumb3}")
+print(f"phone number {p} \ngrade: {gr:.1f}")
