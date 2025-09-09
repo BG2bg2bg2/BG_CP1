2dc8b60d8c5e53c142a46d7fd836c7519ca5bb06
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-09-08 18:56:37 -0600

    Update basic_calculator.py

--- projects.py/basic_calculator.py
+++ projects.py/basic_calculator.py
@@ -8,21 +8,44 @@ def is_n(s):
   except ValueError:
     return False
   
+  
+while True:
+  a1 = input("enter an operator [+ , -, *, /, **, //, %, 'a' for all or (blank) to exit] ")
+  if a1 == "":
+    print("Exiting calc")
+    break
 
-while True:
-    n1 = input("enter a number")
-    if is_n(n1) == False:
-       print("Must be a number")
-    else:
-       break
-while True:
-    n2 = input("enter a second number")
-    if is_n(n2) == False:
-       print("must be a number")
-    else:
-       break
+  while True:
+      n1 = input("enter a number ")
+      if is_n(n1) == False:
+        print("Must be a number")
+      else:
+        n1 = float(n1)
+        break
+  while True:
+      n2 = input("enter a second number ")
+      if is_n(n2) == False:
+        print("must be a number")
+      else:
+        n2 = float(n2)
+        break
+  if a1 == "+" or a1 == "a":
+    print(f"{n1:.2f} + {n2:.2f} = {n1 + n2:.2f}")
+  if a1 == "-" or a1 == "a":
+    print(f"{n1:.2f} - {n2:.2f} = {n1 - n2:.2f}")
+  if a1 == "/" or a1 == "a":
+    print(f"{n1:.2f} / {n2:.2f} = {n1 / n2:.2f}")
+  if a1 == "*" or a1 == "a":
+    print(f"{n1:.2f} * {n2:.2f} = {n1 * n2:.2f}")
+  if a1 == "%" or a1 == "a":
+    print(f"{n1:.2f} % {n2:.2f} = {n1 % n2:.2f}")
+  if a1 == "//" or a1 == "a":
+    print(f"{n1:.2f} // {n2:.2f} = {n1 // n2:.2f}")
+  if a1 == "**" or a1 == "a":
+    try:
+      print(f"{n1:.2f} ** {n2:.2f} = {n1 ** n2:.2f}")
+    except:
+      print("value too large for exponent")
 
-while True:
-    n1 = int(n1)
-    n2 = int(n2)
-    print(n1 {} n2)
+
+    
