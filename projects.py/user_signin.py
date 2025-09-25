4328ea992a163872aa7a740753fc26d60fcfd2ef
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-09-25 02:41:51 +0000

    asdfasdf

--- projects.py/user_signin.py
+++ projects.py/user_signin.py
@@ -1,50 +1,14 @@
 #BG 1st user_signin.py
 uname1 = "Hipo"
 pw1 = "Lolo"
-
+#get username
 user_name = input("Enter your username ")
+
+#get password
 password = input("Enter your Password pls ")
+
+#Welcome if user and pass match
 if uname1 == user_name and pw1 == password:
     print(f"Welcome, {user_name}")
 else:
     print("Invalid credentials aka info.")
-#check for username
-
-#Check for password
-
-#Only output the welcome message if BOTH username and password are correct.
-#
-
-
-#Make sure your code works (test it 5 times with different numbers AND have your elbow partner test it!)
-
-#Save your work
-
-#Commit and push your code to Github
-
-#Not my code
-#           |
-#           |
-#          " "
-#           '
-
-#username6 = "notneeded23"
-#password = "BLJDFI324lkjaekl#$#@lkj"
-#user = input ("What is your username")
-#pword = input ("What is your password")
-#if not user or not pword:
-#    print("Enter a valid input")
-#elif user == "Love" and pword == "passweord":
-#    print("Hello Love")
-#
-#if pword == "passweord":
-#    print("Hello Love")
-#else:
-#    print("Incorect password")
-#    pass
-#if user == username6 and pword == password:
-#    print("wellcom")
-#elif user == username6:
-#    print("Password incorect")
-#else:
-#    print("Incorect password")
