0b31c9121a70da81f403b3bc94359400d505ffd6
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-09-27 05:00:35 +0000

    update at 11 pm

--- projects.py/what_is_my_grade.py
+++ projects.py/what_is_my_grade.py
@@ -1,40 +1,44 @@
 # BG 1st What is my grade
 
+# grade rules for some young aged classes
 #A/ 94 | A-/ 90 | B+/ 87 | B/ 84 | B-/ 80 | C+/ 77 | C/ 74 | C-/ 70 | D+/ 67 | D/ 64 | D-/ 60 | F/ 0
 LG = False
 while LG == False:
+    #Try make the code run unless false
     try:
-        grade = float(input("Enter your grade"))
-        result = (f"Your score of {grade} is ")
-        if grade > 100:
-            #print(f"you have extra credit with {grade - 100}")
-            result = result + "A"
-        elif grade <= 100 and grade >= 94:
-            print(f"You have an 'A' with {grade}%")
+        #This part is an input if one puts in a float it will be valid otherwise it syn error
+        grade = float(input("Enter your grade "))
+        #converts the int into an f statement leaving some words and space for the other words that will follow latter on
+        result_begin = (f"Your score {grade}% is ")
+        #anything greater than 94 is an A no A+
+        if grade >= 94:
+            result_end = "an A"
+        #else is a, "since the previce is False then force this code to run True" but elif is "since the others is/are False then see if this is True or False else move on" 
         elif grade < 94 and grade >= 90:
-            print(f"You have an 'A-'")
+            result_end = "an A-"
         elif grade < 90 and grade >= 87:
-            print("You have a 'B+'")
+            result_end = "a B+"
         elif grade < 87 and grade >= 84:
-            print("you have a B")
+            result_end = "a B"
         elif grade < 84 and grade >= 80:
-            print("You have a 'B-'")
+            result_end = "a 'B-'"
         elif grade < 80 and grade >= 77:
-            print("you have a 'C+'")
+            result_end = "a 'C+'"
         elif grade < 77 and grade >= 74:
-            print("You have a 'C'")
+            result_end = "a 'C'"
         elif grade < 74 and grade >= 70:
-            print("You have a 'C-'")
+            result_end = "a 'C-'"
         elif grade < 70 and grade >= 67:
-            print("You have a 'D+'")
+            result_end = "a 'D+'"
         elif grade < 67 and grade >= 64:
-            print("You have a 'D'")
+            result_end = "a 'D'"
         elif grade < 64 and grade >= 60:
-            print("You have a 'D-'")
+            result_end = "a 'D-'"
         else:
-            print("You have an 'F'")
+            result_end = "an 'F'"
+        #stops the wile loop from restarting and moves onto the nonindented parts connected to the While loop.
         break
     except:
         print("try again")
+print(result_begin + result_end)
 
-    print(result)
