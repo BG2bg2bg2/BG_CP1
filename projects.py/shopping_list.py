6c8732fbb644ead158770c092f93493d3955abda
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-10-01 04:03:03 +0000

    adsf

--- projects.py/shopping_list.py
+++ projects.py/shopping_list.py
@@ -2,31 +2,38 @@
 
 shop = []
 while True:
-    print("\n1 = add item.\n2 = remove item. \n3 = show list.\n4 = done")
-    num = input("\nchoose a number 1-4 ")
+    print("\n1 = add item.\n2 = remove item. \n3 = show list.\n4 = clear. \n5 = Quit")
+    num = input("\nchoose a number 1-5 ")
 
     if num == "1":
         item = input("\nwhat is the item you want to add? ")
         shop.append(item)
         print(f"\n{item} has been added")
 
-    elif num == "2":
+    if num == "2":
         item = input("\nwhat item do you want to remove? ")
         if item in shop:
             shop.remove(item)
             print(f"\n{item} removed")
         else:
-            print("\n{item} not found")
+            print(f"\n{item} not found")
 
-    elif num == "3":
+    if num == "3":
         if shop:
-            for a in enumerate(shop, 1):
-                print(f"\n{a}, {item}")
+            for a in enumerate(shop):
+                #print({item}, {a})
+                print("\n not going to work")
+                print(list(shop))
         else:
             print("\nNo item on")
 
-    elif num == "4":
-        print("\nquiting")
+    if num == "4":
+        print("\nclear")
+        if shop:
+            shop.clear()
+            print("cleared")
+    if num == "5":
+        print(f"\nQuiting")
         break
     else:
         print("\ntry again")    
