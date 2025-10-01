5fd0c175c5a0333a2432990ee411d91c816afc89
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-10-01 09:53:07 -0600

    adfadsf

---
+++ projects.py/multiplacation_table.py
@@ -0,0 +1,8 @@
+#BG 1st Multipacation table
+print("welcome to multiplactaion tables")
+print("Rows are up and down while colomes are left and right")
+table_colum = input("enter a number for colums going left to right")
+if table_colum.isalpha():
+    print("must be anumber")
+if table_colum.isdigit():
+    table_row = input("enter another number for rows going up and down")
