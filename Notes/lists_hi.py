5c509d339b067f2c2d67e19465f987dbd904e977
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-10-02 09:54:31 -0600

    asdf

--- Notes/lists_hi.py
+++ Notes/lists_hi.py
@@ -2,30 +2,35 @@
 import random
 not_sibs = ["mom", "dad", "Ammon", "Fred", "Nielsen", "Russle", "Gabrel", "Alma", "Nephi", 5.76, False]
 
+#for a in not_sibs:
+    #print(not_sibs[5])
+    #print(random.choice(not_sibs, weights=[10, 10, 10, 10, 10,10], k=5))
+    #print(f"the list is, {len(not_sibs)} people long")
+    #print(not_sibs)
+    #replacing is this
+   # not_sibs[0] = "Eric"
+   # #not this
+   # #not_sibsreplace("Fred", "Parker") this only works for strings
 
-print(not_sibs[5])
-print(random.choice(not_sibs, weights=[10, 10, 10, 10, 10,10], k=5))
-print(f"the list is, {len(not_sibs)} people long")
-print(not_sibs)
-#replacing is this
-not_sibs[0] = "Eric"
-#not this
-#not_sibsreplace("Fred", "Parker") this only works for strings
+#    not_sibs[6:-1] = ["lavender", "Green"]
+ #   not_sibs.pop()
+ #   not_sibs.pop(6)
+  #  not_sibs.remove("dad")
 
-not_sibs[6:-1] = ["lavender", "Green"]
-not_sibs.pop()
-not_sibs.pop(6)
-not_sibs.remove("dad")
+    #not_sibs clear()
+    #not_sibs.append("Fred") adds to the end of the list
+    #not_sibs.insert(2, "Anderwel")
+    #adds things the the end of the list
+   # not_sibs.extend(["Joseph", "Isreal", "USA"])
+   # print(not_sibs)
+   # continue
+#else:
 
-#not_sibs clear()
-#not_sibs.append("Fred") adds to the end of the list
-not_sibs.insert(2, "Anderwel")
-#adds things the the end of the list
-not_sibs.extend(["Joseph", "Isreal", "USA"])
-print(not_sibs)
 
-board = [[ 1,2,3]
-         [4,5,6],
-         [7,8,9]
+board = [[1|2|3],
+         [4|5|6],
+         [7|8|9]
         ]
 
+for a in enumerate(board,1):
+    print(board)
--- projects.py/multiplacation_table.py
+++ projects.py/multiplacation_table.py
@@ -1,18 +1,14 @@
 #BG 1st Multipacation table
 print("welcome to multiplactaion tables")
 print("Rows are up and down while colomes are left and right")
-table_colum = input("enter a number for colums going left to right")
-if table_colum.isalpha():
-    print("must be a number")
-if table_colum.isdigit() and table_colum <= 12:
-    table_row = input("enter another number for rows going up and down")
-    if table_row.isalpha() and table_row <= 12:
-        print("Must be a number")
-        if table_row.isdigit():
-            print("Continuing")
-        
-else:
-    print("Something is wrong :(")
-mt = False
-while mt == False:
-    Mt = input
+
+
+table_colum = [1| 2| 3| 4| 5| 6| 7| 8| 9| 10| 11| 12]
+a = table_colum
+b = table_colum
+print(a)
+for b in table_colum:
+    print(b)
+
+for a in enumerate(table_colum):
+    print(table_colum)
