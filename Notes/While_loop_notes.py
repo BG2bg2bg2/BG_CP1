58b086fe140fc6dcb2de17ee8ed9b80009934ba2
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-10-03 09:42:13 -0600

    adsf

--- Notes/While_loop_notes.py
+++ Notes/While_loop_notes.py
@@ -7,3 +7,29 @@ num = 1
 while num <=20:
     print(num)
     num+=1
+
+import random
+goose = random.randint(1,20)
+count = 0
+while count != goose:
+    count += 1
+    print("duck")
+    """if count == goose:
+        break
+    else:"""
+    if count == 13:
+        break
+else:
+    print("GOOSE")
+print("code is done")
+
+
+i = 0
+while i <= 20:
+    if i == 10:
+        print("i is 10")
+        continue
+    else:
+        print(f"{i} iteration of the code")
+        i+= 1
+print("the loop ended")
