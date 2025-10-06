b8a03dfa83b9f993f16b8a580b5cefb867172393
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-10-05 21:03:08 -0600

    Update multiplacation_table.py

--- projects.py/multiplacation_table.py
+++ projects.py/multiplacation_table.py
@@ -1,9 +1,19 @@
-#BG 1st Multipacation table
-print("welcome to multiplactaion tables")
-print("Rows are up and down while colomes are left and right")
+#BG 1st multiplacation table
 
-row = [1,2,3,4,5,6,7,8,9,10,11,12]
-colum = 12
-
-for i in row:
-    print(i)
+while True:
+    print("Welcome to the multiplication table")
+    colum = input("Enter a number for colum 1-12 ")
+    row = input("enter a number for row 1-12 ")
+    if not colum.isdigit() or not row.isdigit():
+        print("Must be a number")
+        continue    
+    y = int(colum)+1
+    if row.isdigit():
+        x = int(row)+1
+        if x > 22 or y > 22:
+            print("both must be <= 21")
+            continue
+        for q in range(1, x):
+            for d in range(1, y):
+                print(f"{q*d:>{5}}",end="")
+            print()
