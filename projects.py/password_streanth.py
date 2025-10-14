d1233aede99a0086fef4a0cfd5d8a9bdcf1a3bfa
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-10-14 01:00:31 +0000

    asdasassaasdasda

--- projects.py/password_streanth.py
+++ projects.py/password_streanth.py
@@ -1,9 +1,6 @@
-#BG 1st pseudocode pass checker
+#BG 1st pass checker
 
 
-#user score == 0 
-
-user_score = 0
 
 #spectial charitcers
 spectial_chericers = "!@#$%^&*()_+~`_+-={}|[];':<>?,./\\"
@@ -15,38 +12,50 @@ Upper = "QWERTYUIOPLKJHGFDSAZXCVBNM"
 
 #for the index latter on to see if pass has any of these
 lower = "qwertyuioplkjhgfdsazxcvbnm"
-#enter a password
-user_pass = input("enter pass")
+
 
 #Loop until false
 code = True
 while code != False:
+    #user score is cleared each run
+    user_score = 0
+    #enter a password
+    user_pass = input("enter password ")
 #check to see if there is numbers
     for i in user_pass:
         if i in num:
             #user score + 1 if true
             user_score = user_score + 1
-            continue
+            break
 #check to see if there is lower letters
     for l in user_pass:
         if l in lower:
             #user score + 1 if ture
             user_score = user_score +1
-            continue
+            break
 #check to see if there is Upber letters
     for a in user_pass:
         if a in Upper:
             user_score = user_score +1
-            continue
+            break
 #check if there is special characters
     for b in user_pass:
-        if a in spectial_chericers:
+        if b in spectial_chericers:
             user_score = user_score + 1
-            continue
+            break
 #check to see if the password is 8 long
-    for c in user_pass:
-        if c in len(user_pass) >= 8:
-            user_pass = user_pass + 1
-            continue
+    if len(user_pass)>= 8:
+        user_score += 1
+    
+    #if the score is equivelint to any of these it will tell if the pass is week-strong in streanth wise
     print(user_score)
-    code == False
+    if user_score == 0:
+        print("No blank password alowed")
+    elif user_score <= 2:
+        print("weak")
+    elif user_score == 3:
+        print("moderate")
+    elif user_score == 4:
+        print("strong")
+    elif user_score == 5:
+        print("Very strong")
