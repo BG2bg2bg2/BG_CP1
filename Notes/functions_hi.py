7fbeffafc3c0188656fcc38f3fa6abeeaccebc8d
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-10-20 09:53:35 -0600

    asdf

--- Notes/functions_hi.py
+++ Notes/functions_hi.py
@@ -1,7 +1,24 @@
 #BG 1st function notes
 
-hi = "1"
+#def = definitoins
 
-ijlklkjk = 1
 
-af = "af"
+def add(x,y):
+    return x+y
+
+def initaials(name):
+    names = name.split(" ")
+    initials = ""
+    for name in names:
+        initials += name[0]
+    
+    return initials
+
+total = add(5,5)
+print(total)
+print(f" 10 + 72 = {add(10,72)}")
+x = 0
+while x < add (3,9):
+    print("duck")
+    x += 1
+add(3,9)
