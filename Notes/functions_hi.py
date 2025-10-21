b993dbfbd317923cc5012a1ddf61c558deb32701
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-10-21 09:53:12 -0600

    10.21

--- Notes/functions_hi.py
+++ Notes/functions_hi.py
@@ -1,12 +1,27 @@
 #BG 1st function notes
 
+
+p_health = 100
+m_health = 100
+def damage(amount, turn):
+  if turn == "player":
+    return m_health - amount, p_health
+  else:
+     return m_health, p_health - amount
+
+m_health, p_health = damage(10, "player")
+print(f"{m_health} montster health")
+print(f"{p_health} player health")
+
+
+
 #def = definitoins
 
 
 def add(x,y):
     return x+y
 
-def initaials(name):
+def initials(name):
     names = name.split(" ")
     initials = ""
     for name in names:
@@ -22,3 +37,8 @@ while x < add (3,9):
     print("duck")
     x += 1
 add(3,9)
+print("Goose!")
+print(initials("Brett Gerlach"))
+
+print(f"a = {ord("a")} ")
+print(f"98 = {chr(98)}")
