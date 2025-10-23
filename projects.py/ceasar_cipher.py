b1c105c12254913e0dead62c192a2afc6ddd772d
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-10-23 02:36:56 +0000

    10.22.25 at 8.36

--- projects.py/ceasar_cipher.py
+++ projects.py/ceasar_cipher.py
@@ -1,76 +1,87 @@
 #BG 1st ceasar cipher
 
-list_code = []
 
 def encode(phrase, shift):
     new_phrase = ""
-    shift = 1
     # Return error message if shift > 25 (there are 26 characters in the alphabet) or < 0
     # step through each character of the string
     for a in phrase:
         # get its ASCII value with ord(character) to get ASCII value
         old_ascii = ord(a)
-        print(old_ascii)
         # Find out if lowercase or uppercase
         if old_ascii >= 65 and old_ascii <= 90:
             b = old_ascii + shift
             if b > 90:
                 b -= 26
-            print(b)
             new_phrase += chr(b)
         elif old_ascii >= 97 and old_ascii <= 122:
             c = old_ascii + shift
             if c > 122:
                 c -= 26
-            print(c)
             new_phrase += chr(c)
         else:
             new_phrase += a
         # add shift to the ASCII value
+    return new_phrase
+
+def decode(phrase, shift):
+    new_phrase = ""
         
+    # step through each character of the string
+    for a in phrase:
+        # get its ASCII value with ord(character) to get ASCII value
+        old_ascii = ord(a)
+        # Find out if lowercase or uppercase
+        if old_ascii >= 65 and old_ascii <= 90:
+            b = old_ascii - shift
+            if b < 65:
+                b += 26
+            new_phrase += chr(b)
+        elif old_ascii >= 97 and old_ascii <= 122:
+            c = old_ascii - shift
+            if c < 97:
+                c += 26
+            new_phrase += chr(c)
+        else:
+            new_phrase += a
+
         # check to see if past Z or z and wrap to beginning of alphabet if needed
     
     # add character to new string  with chr(ascii)
     # return new phrase
     return new_phrase
 
-i = input("hi")
-i = (f"{new_phrase}")
-print()
 
-code = True
 # make sure the code can run till code == False
-while code != False:
+while 1 == 1:
     # meant to help guild the user to know what to do
-    print("welcome to ceasar cipher enter a number 1-5 \n 1 = enter letters to encrypt with shift defalt as 1 \n 2 = enter numbers to encrypt with shift defalt as 1 \n 3 = enter letters and choose how far you want to shift 25 is max \n 4 = enter numbers to encrypt and choose how far you want to shift 25 is the max \n 5 = quit")
+    print("welcome to ceasar cipher enter letters \n e = encode \n d = decode \n q = quit")
     
-    # enter a number
-    choice = (input("enter a number"))
-    
-    # for what is in choose depends on the out come
-    for d in choice:
-        try:
-            choose = int(choice)
-        except:
-            print("try again")
-            continue   
-        if choose == 1:
-                code1 = input("enter letters to encrypt defalt of 1")
-                if code1.isalpha():
-                    code1 = (new_phrase, shift)
-                    print(code1 + 1)
-                elif code1.isdecimal() or code1.isdigit():
-                    print("try again")
-        
-        if choose == 2:
-            for e in list_code:
-                code1 = (input("enter numbers to encrypt with defalt of 1"))
-                list_code.append(code1)
-    
-        if code1 is code1.isdigit():
-            c = chr(code1)
-            print(c)
-        elif code1 is complex():
-            d=ord(code1)
-            print(d)
+    # enter a letter
+    choice = input("enter a letter e, d, q: ")
+    if choice != "q" and choice != "e" and choice!= "d":
+        print("try again")
+        continue
+    else:
+        if choice == "q":
+            break
 
+    # for what is in choose depends on the out come
+    
+        phrase1 = input("enter phrase: ")
+        shift1 = input("enter how many letters to shift: ")
+        if shift1.isdigit():
+            # Return error message if shift > 25 (there are 26 characters in the alphabet) or < 0
+            shift1 = int(shift1)
+            if shift1 > 26:
+                #code wont run if true 
+                print("must be btween 0 and 26")
+                #restart the prosess
+                continue
+            else:
+                if choice == "e":
+                    #send letters forward
+                    print(encode(phrase1, shift1))
+                elif choice == "d":
+                    #send letters backward.
+                    print(decode(phrase1, shift1))
