06de9b8af62956ccd31b40da4e1ba6c819fcea76
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-10-26 03:35:14 +0000

    10.25.25

--- projects.py/combat.py
+++ projects.py/combat.py
@@ -1,26 +1,70 @@
 #BG 1st combat DD virstion
 
-import time
 import random
 
-list_of_charactures = []
-charactures = ['roge', 'ranger', 'knight', 'mage']
-stragth = [10, 60, 90, 100]
-realowed_time = [1, 5, 3, 10]
-health = [40, 30, 100, 40]
 
-for i in range(len(list_of_charactures)):
-    user = {"charactur": charactures[i], 'strangth': stragth[i], "slow time": realowed_time[i], "life": health[i]}
-keys = ['characture', 'strangth', 'slow time', 'life']
-values = [
-    ['roge', 10, 1, 40],
-    ['ranger', 60, 5, 30]
-    ['knight', 90, 3, 100]
-    ['mage', 100, 10, 40]
-]
 
-i = input('enter a number')
-for i in range(len(list_of_charactures)):
-    user_choice = user
+class user:
+    def __init__ (self, class_name):
+        self.class_name = class_name
+        self.health = 30
+        self.defense = 3
+        self.attack = 20
+        self.potion = 12
+        self.speed = 10
+    
+    def display_stats(self):
+        print(f"{self.class_name}")
+        print(f"health = {self.health}")
+        print(f"defense = {self.defense}")
+        print(f"attack = {self.attack}")
 
-print(user)
+    def do_attack(self):
+        attack_strength = (random.randint(1, self.attack))
+        return attack_strength
+    
+    def do_defend(self, attack_strength):
+        defense_block = random.randint(1, self.defense)
+        lost = attack_strength - defense_block
+        if lost < 0:
+            print("you blocked the attack!")
+        else:
+            self.health -= lost
+            print(f"you lost {lost}. Now you have {self.health} health.")
+
+    def drink_potion(self):
+        health_gain = random.randint(1, self.potion)
+        self.health += health_gain
+        print(f"you gained {health_gain} now you have {self.health}")
+
+    def super_attack(self):
+        attack_strength = 2*random.randint(1, self.attack)
+        print(f"you did {attack_strength} damage by using your weight")
+        lose_life = attack_strength / random.randint(1, self.health)
+        a = round(lose_life)
+        self.health -= a
+        print(f"fun you lost {a} health now you have {self.health}")
+
+    def flee(self):
+        run = random.randint(1, self.speed)
+        if run == self.speed():
+            print("you ran with spikes on your heels and slowly killing the monster at your feet")
+            print("YOU SUCCESSFULY WON THE GAME!")
+            print("GAME OVER YOU WON")
+
+        elif run <= 2/self.speed():
+            print("You were climbing a mountain and making a land slide killing the monster but cutting yourself on jaged edges")
+            print("YOU beat that monster but now you face a new monster on the mountain")
+            print("want to continue your quest or jump off?")
+            print("Depends on how you look at it!!!")
+        else:
+            print("You died by falling in a pit that was behind a log that you, yourself has setted up")
+            print("You DIE GOOD LUCK NEXT TIME (if there is a next time)")
+
+player = user("Knight")
+player.display_stats()
+pa = player.do_attack()
+print(f"your attack was {pa}")
+player.do_defend(14)
+player.drink_potion()
+player.super_attack()
