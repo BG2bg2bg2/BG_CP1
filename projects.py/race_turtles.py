fe986d34f9600c9c2c0076c0bacc26cb2d82f6f7
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-10-30 09:43:34 -0600

    200

--- projects.py/race_turtles.py
+++ projects.py/race_turtles.py
@@ -5,31 +5,58 @@ import random
 import time
 
 
+
+#makes the screen
 screen = turtle.Screen()
-screen.setup(600, 400)
+screen.setup(900, 900)
 screen.title("Racing Five Turtles")
 
+#colors for the turtles
+acolors = ["red", "yellow", "blue", "purple", "green", "orange", "brown"]
+#assines the colors
+colors = random.sample(acolors, 5)
 
-colors = ["red", "yellow", "blue", "purple", "green", "orange", "brown"]
+
+#how many steps the turtls take
 step = [1,2,3,4,5,6,7,8,9,10]
-r = random.choice(len(step))
-distence = 200
-speed = 50
-delay = speed / step
 turtles = []
+
+
+
+#Makes the turtles
 
 for i in range(5):
     t = turtle.Turtle()
-    t.color(random.choice(colors))
+    t.shape("turtle")
+    t.color(colors[i])
     t.penup()
     t.goto(-250, 100 - i * 50)
     turtles.append(t)
 
-for i in range(distence // step):
-    t.forward(step)
-    time.sleep(delay)
-    
-    
+# Marks the finish line
+a = turtle.Turtle()
+a.hideturtle()
+a.speed(0)
+a.penup()
+a.goto(500, 250)
+a.pendown()
+a.pensize(5)
+a.color("black")
+a.right(90)
+a.forward(500)
+
+finish_line = 500
+
+
+race = True
+while race:
+    for t in turtles:
+        t.forward(random.choice(step))
+        if t.xcor() >= finish_line:
+            race = False
+            print(f"The {t.pencolor()} turtle won!")
+            break
+    time.sleep(.01)
 
 turtle.done()
 
