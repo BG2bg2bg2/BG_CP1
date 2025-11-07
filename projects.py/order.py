d21c51186c0ca7b19affcc96026a57d6f283f2af
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-11-07 09:53:15 -0700

    12321

---
+++ projects.py/order.py
@@ -0,0 +1,13 @@
+#BG 1st order python
+
+#Create a program that allows a user to order items off a menu.
+#  Menu items need to be stored in a dictionary with their prices as the value.
+#  Users need to be able to order a drink, a main course and two side dishes. 
+# At the end the program needs to repeat back to the user their full order, and the total cost. 
+drink = [num_drinks]
+
+cost = "hi"
+
+def num_drinks():
+    for a in drink:
+        d.list()
