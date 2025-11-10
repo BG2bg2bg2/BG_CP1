abc9775f761274ea93c4bf0cd5fd87ea89a02cd6
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-11-10 09:51:05 -0700

    adsf

--- Notes/dictionary_notes.py
+++ Notes/dictionary_notes.py
@@ -2,50 +2,7 @@
 
 #What are dictionaries?
 
-#key: value pairs
-person = {
-    #key:value,
-    "name": "Xavier",
-    'age': 22,
-    'job': "Mavrick",
-    'sibling': ["Alex", "Katie", "andrew", "Vienna", "Tia", "Treyson", "Jake"]
-}
-person_two = {
-    "name": "Alex",
-    'age': 14,
-    'job': "Mavrick",
-    'sibling': ["Xavier", "Katie", "andrew", "Vienna", "Tia", "Treyson", "Jake"]
-}
-print(f"Name is {person["name"]}")
-print(person.keys())
 
-
-for key in person.keys():
-    if key == "sibling":
-        for name in person[key]:
-            print(f"{person['name']} has a sibling named {name}")
-        else:
-            print(f"{key} is {person[key]}")
-
-print(person.values())
-person["age"] += 1
-print(person.items())
-print(person.keys())
-person_two["SO"] = "Carry"
-person["age"] += 1
-print(person_two.items())
-
-avatar = {
-    "earth": {
-        "toph": "Sounds like Tough and thats just what I am!"
-    },
-    "Water": {
-        "Katara": "Itt just game me so much Hope",
-        "Sokka": "I used to be boomerang guy"
-    }
-}
-
-print(avatar['earth']['toph'])
 #How do dictionaries simply variables in a project?
 
 
--- projects.py/order.py
+++ projects.py/order.py
@@ -1,9 +1,12 @@
 #BG 1st order python
 
+cost = .76
+
 #Create a program that allows a user to order items off a menu.
+
+
 #  Menu items need to be stored in a dictionary with their prices as the value.
-#  Users need to be able to order a drink, a main course and two side dishes. 
-# At the end the program needs to repeat back to the user their full order, and the total cost. 
+
 num_drinks = {
     "root beer": .92,
     "Mountain dew": .23,
@@ -12,5 +15,13 @@ num_drinks = {
 }
 drink = [num_drinks]
 
-cost = .76
 
+#  Users need to be able to order a drink, a main course and two side dishes.
+
+for a in num_drinks:
+    choice = input("Enter a leter a-f")
+    print("a = choose a drink")
+    if choice == "a" or choice == "A":
+        print(num_drinks["root beer"])
+
+# At the end the program needs to repeat back to the user their full order, and the total cost. 
