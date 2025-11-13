0311898ff6ea6eef5608d64451e907d2c7496cd9
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-11-13 02:17:34 +0000

    asda

--- projects.py/order.py
+++ projects.py/order.py
@@ -2,48 +2,54 @@
 
 tax = .5
 #food for the sides
+#dictonary
 sides = {
-    "fries": {"cost": 3, "amount": 0},
-    "chicken nuggets":{"cost": 2.3, "amount": 0},
-    "roles": {"cost": 2, "amount": 0},
-    "muffins": {"cost": 2.9, "amount": 0}
+    #diction in dictionary
+     "1": {"name": "fries", "cost": 3},
+    "2": {"name": "chicken nuggets", "cost": 3},
+    "3": {"name": "roles", "cost": 3},
+    "4": {"name": "muffins", "cost": 3}
 }
 #main food corse
 main_corse = {
-    "hamberger": {"cost": 3, "amount": 0},
-    "cheesberger": {"cost": 2.3, "amount": 0},
-    "salad": {"cost": 3.7, "amount": 0},
-    "PBJ": {"cost": 4, "amount": 0}
+    "hamberger": {"cost": 3},
+    "cheesberger": {"cost": 2.3,},
+    "salad": {"cost": 3.7},
+    "PBJ": {"cost": 4}
 }
 #drinks
 drinks = {
     "root beer": .4,
     "sprite": .9,
     "MTN Dew": .8,
-    "Coca Cola": 2.1 
+    "Coca Cola": 2.1
 }
 
+def display_menu(menu):
+    for a in menu.keys():
+        print(a, menu.get(a).get("name"))
 
-shop = []
+order = []
 while True:
-    
+    display_menu(sides)
+    break
     print("\n1 = choose from the sides\n2 = choose from the main corse. \n3 = choose from the drinks.\n4 = check out")
     num = input("\nchoose a number 1-5 ")
 
     if num == "1":
         print("\nCurrent sides:", sides)
-    action = input("enter 'add', 'pay', or 'next': ").lower()
+    action = input("enter '1' add, '2' dislay menu, or '3' next: ").lower()
 
-    if action == "next":
+    if action == "1":
         continue
     elif action in ['add', 'pay']:
-        order = input("enter your order: ")
+        item = input("enter your order: ")
         quantity = int(input("enter quantity: "))
 
         if action == "add":
-            sides[shop] = order
+            sides[order] = ite
         else:
-            if sides in order:
+            if sides in item:
                 shop.append(order)
             else:
                 print("Error: not enough stock or book not found")
@@ -77,4 +83,4 @@ while True:
         print(f"\nQuiting")
         break
     else:
-        print("\ntry again")    
+        print("\ntry again")
