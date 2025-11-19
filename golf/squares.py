ec9aa7c77a95bad7e7cd9d732e4b374f4338b5d8
BG2bg2bg2 <126535862+BG2bg2bg2@users.noreply.github.com>
2025-11-19 12:04:24 -0700

    adf
    Updated the list of numbers and modified the mapping to create a dictionary of original numbers and their squares.

--- golf/squares.py
+++ golf/squares.py
@@ -1,10 +1,13 @@
 #BG 1st squares
 
 #list of numbers
-numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
-#map does 
-pair = map(lambda num: (f"original: {num}", f"squared: {num**2}"), numbers)
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
+#map and lambda work to gether to give the output of numbers
+                #pair puts the original numbers next to the squared numbers from list of numbers
+pair = map(lambda num: (f"original: {num}", f"squared: {num**2}\n"), numbers)
 
-squared = dict(pair)
+#both returns what is in pair to be availible to make the list and gives a dictionary
+both = dict(pair)
 
-print(squared)
+#display what the previous code made
+print(both)
