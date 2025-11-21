5a639913267017d45c8557f33e991f1d6ad614ef
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-11-21 09:53:04 -0700

    adsfadsf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

---
+++ projects.py/psudoecode_partner.py
@@ -0,0 +1,28 @@
+#BG 1st psudeocode partner
+
+#display("welcome to the factorial calculator")
+print ("welcome to the factorial calculator")
+#number = input(number) original = number
+number = input(int)
+# if number < 0
+if number < 0:
+        #display "please dont use negatives"
+        print("please don't use negatives")
+#else if input is a float:
+elif number.():
+    #display "please dont use decimals"
+#elif input is >0 and not a decimal
+        #pass
+    #else:
+        #invalid
+
+#number = []
+#while number >1
+    #number - 1
+    #number.append(number)
+    #if number == 1
+    #display("calculating")
+    #for num in numbers 
+    #num = numbers all multiplied with each other
+
+#display number
---
+++ starters.py/what.py
@@ -0,0 +1,23 @@
+#BG 1st
+
+from turtle import *
+
+def draw_branch(length):
+    if length > 5:
+        for _ in range(3):
+            t.forward(length)
+            draw_branch(length/3)
+            t.backward(length)
+            t.right(60)
+t = Turtle()
+t.speed(3)
+t.color("light_blue")
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+for i in range(6):
+    draw_branch(100)
+    t.right(60)
+
+t.done()
