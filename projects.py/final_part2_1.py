c8487c78cbe5858657ebe1f18f27ffbb9b6bccf0
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-12-01 09:51:38 -0700

    asdf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

---
+++ projects.py/final_part2_1.py
@@ -0,0 +1,130 @@
+#BG 1st psudocode
+
+""" import libraryies needed
+#I will have 4 options to ask the user to have
+
+strength defalt
+
+speed defalt
+
+mass defalt
+
+power defalt
+
+energy defalt
+
+stealth defalt
+
+user inputs 1-4
+
+2 defend
+3 attack
+1 search
+
+
+#Search for lost people (hire someone to make a space ship for others to find the lost people and collect ores to make a bigger ship with guns to attack the boss but one will sabotage the weapons) 
+
+    disply your incharge of searching but need to make something stelthy but you realise that the next time the enemy comes is later but you think it is sooner what do you do
+
+
+#Defend your people (you send people then wait a time and they either die, return and bring people, or ores and you use those to try to bring the boss to you but someone will call someone that will help at the end or call the boss sooner than planned)
+#Attack the boss (hire someone to make a space ship with what you have available, collect ores, and go to planets in the habitable zone to add on to the ship but someone that is with you is a spy)
+
+
+#Earth (use what you have or make more using what you have found but some people are spies)
+defalt
+
+build a space ship with companies of your choice
+
+SpaceX is about speed, transport, men, stealth, and wepons
+
+NASA is about travle, weapons, max mass, stealth, and mining
+
+ISTMTFWAACEO (International Space Transportation Mining Travle Fast Weapons that are Always Availible Company Economy Oporation) is about everything evenly spaced exept boss uses spies in this one
+    travle, weapons, transport, men, speed, cargo, stealth none to baraly
+
+#Industry (Research alloys to make ships light, fast, and strong)
+
+
+
+#Rockets (Make a ship to hold what you will use)
+#Weapons (Put weapons and tools to mind ores to make better tools or weapons)
+
+#LA (A planet far far away but has more abundant ores that are better for use)
+#A spaceship that is abandoned because it has a hole in the center but chose to plunder the ship or tow it or fix it or leave it
+#An abandoned industry with plenty of resources to upgrade your ship
+#A mountain with different ores but can only mine one because of time
+
+#UA (A galaxy were you will find some people that have information on the boss)
+#You get a signal and you don't know if they are in distress or if they are trapping you.
+#You get some colors that you recognise as excitement and jackpot but some say you should reply others say you should run
+#You find a station with someone you recognise who you know is rebellious to everyone they meet but confused that they are posted with a 1st place metal on a screen and you see that picture of the person on another screen on a ship going to the station
+
+#Quip (A space ship that was abandoned but the objects are useful to you and the boss)
+##You see a ship that shows no life forms but you recognise there are weapons that are useful
+#Plunder
+#Tow
+#Fire at it then plunder
+#Collect what you can off of it then leave
+#LASS (A space station that manufacturer things that could be useful)
+#You park there but in order to keep you ship you must insert a pin, swipe a card, make an account, or leave
+#Swipe a card that you have
+#Type a pin that you  guess
+#Make a new account
+#Leave and hope you don't die or raise an alarm
+#If you succeed you have free range to upgrade your ship size, power, speed, tools, and weapons, but you can only pick a few upgrades.
+#IPLA (A galaxy that has 5 stations but you can only visit three because of time or your captured by them unless you have upgraded you ship)
+#There will be a logic problem involved
+
+BLAT(The galaxy that has boss dwellings on planets)
+You find a galaxy that has planets with ships that you don't recognise but you know that the boss must have been here before.
+You have the option to attack, invest, or act like your captured if you have the other spaceship to sneak in to make a rebellion against the boss
+UIT (a planet that has a map of the universe to find/call/kill the boss
+You find a ship at the edge of a planet you investigate and collect 3 of 6 items
+One is a map
+One is a create that can only open with a pin
+One is a switch that looks like a rocket
+One has weapon designs
+One has tool designs
+One is a book, possibly a journal of someone's life but you don't know the language it is in.
+HIPOS (Heave Into Planet Operation Station to find out why the boss needs all of these people and things)
+You run into the patrol on your way home and have the option to
+Attack
+Run
+Give what they want or find what they want
+Tell them a lie after they give you info on the boss
+Tell them the truth before they give you info on the boss
+
+You gained info and depending of what you told them the boss
+Comes when your ready
+Hires to kill you
+Send men to inform you why he is collecting people.
+Makes an agreement to return everyone that he collected home after a time
+Pulls you into a trap and you must find out how to escape alive with your men.
+
+
+Characters:
+You as general (“____”)
+Your “best” friend (“Steave”)
+Your “best” friend's friend (“Shoive”)
+Your “best” friend's enemy (“Trape”)
+Your “best” enemy (“Tetipe”)
+Your trusted spaceship man (“Salaxion”)
+Your trusted weapon designer (“Willopon”)
+Your trusted miner (“Minoker”)
+Strangers (“Bob”, “Hape”, “Sape”, “Quope”, “Dupper”, “Maipsutoster”, and “Tom”)
+
+Items:
+Ore (“Gold”, “iron”, “steel”, “rioptor”, “Gas tanks”)
+Weapons (“Lazer”, “Cannons”, “rockets”, “SMT [Space Mines Trouble]”, “Kamacazy Import Space Ships [KISS]”)
+Tools (“Drill”, “Pump”, “net”, “Cargo bay”, “creates” and “Cable”)
+Books (stuff that will help later on)
+Keys (to open creates and books)
+
+Ships
+AESR (“Atack Earth Space Rocket
+MESR (“Mine Earth Space Rocket”)
+SESR (“Search Earth Space Rocket”)
+RESR (“Runner Earth Space Rocket”)"""
+
+
---
+++ projects.py/final_part2_2.py
@@ -0,0 +1,5 @@
+#BG 1st psudeocode
+
+#import libraries
+
+#display
