03032ec3b95644c07a6356d87b5588c4524b1ec9
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-12-02 09:53:19 -0700

    adsf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- projects.py/final_part2_2.py
+++ projects.py/final_part2_2.py
@@ -1,5 +1,47 @@
 #BG 1st psudeocode
 
-#import libraries
+#import libraries: random, time, statistics (maybe), and some others if aproved [os, sys, json]
 
-#display
+#os
+# gives access to operating system features
+# useful for clearing the screen (os.system("cls")) or managing save files
+
+#sys
+# gives control over the Python system itself
+# helpful for exiting the game (sys.exit()) or reading command-line arguments
+
+#json
+# lets you save and load player data in easy-to-read files
+# perfect for inventory, stats, quests, settings, or save files
+
+
+#list of player's ship as user_ship[
+    #stealth = 0,
+    #power = 0,
+    #speed = energy - mass^2,
+    #defence = mass + stength,
+    #stealth = 0,
+    #mass = 0,
+    #stength = 0]
+
+
+#disply
+    #there is an UFO and you see people floating and being pulled into cargo holds but you managed to find a place with others inside a small compartment but you lost some people you loved
+    #you exit but you don't know what to do exept finish up your schooling
+
+#ask user
+#
+
+#funtion earth():
+    #items
+
+#disply
+"Search for lost people (hire someone to make a space ship for others to find the lost people and collect ores to make a bigger ship with guns to attack the boss but one will sabotage the weapons)"
+
+#disply
+"your incharge of searching but need to make something stelthy but you realise that the next time the enemy comes is later but you think it is sooner what do you do"
+
+
+
+#while choice works
+    #user chooses a number
