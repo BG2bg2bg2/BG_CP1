27ecf121cc600f0b2878f291ae2dfd770bedc36d
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-12-03 09:52:20 -0700

    asdf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- projects.py/final_part2_2.py
+++ projects.py/final_part2_2.py
@@ -14,26 +14,95 @@
 # lets you save and load player data in easy-to-read files
 # perfect for inventory, stats, quests, settings, or save files
 
+#stealth = 0,
+#power = 0,
+#speed = energy - mass^2,
+#defence = mass + stength,
+#stealth = 0,
+#mass = 0,
+#stength = 0,
+#ors per turn = 24
+
 
 #list of player's ship as user_ship[
-    #stealth = 0,
-    #power = 0,
-    #speed = energy - mass^2,
-    #defence = mass + stength,
-    #stealth = 0,
-    #mass = 0,
-    #stength = 0]
+
+    #stealth,
+    #power,
+    #speed,
+    #defence,
+    #stealth,
+    #mass,
+    #stength,
+    #ors per turn]
+
+#list of rooms[
+# Earth,
+# La,
+# UA,
+# Quip,
+# LASS,
+# IPLA,
+# BLAT,
+# UIT,
+# HIPOS]
+
+#funtion of player turns
+    #if player on earth
+        #if player has 5 ores or more
+            #if player does not have boosters
+            #ask user if they want to make the boosters for the ship (y/n)
+            #if yes
+            #update 
+        #disply list of rooms and remove Earth
+        # aske user to go to a diffrent room 
+    #else if player on LA
+        #
+
+#funtion of what is on earth:
+    #items are
+        #24 ores from mountains every turn
+            #if ores are not collected after 3 turns
+                #collect ores
+        #a space company you want to team up with (only can choose one through the intire game)
+        #Intel that you found in a jurnal and a heavy chest from people who where taken but you can't understand or open without the keys needed
+    #disply
+    #you can go to the mountains with some men and start collecting everything you see to work on your spaceship
+
 
 
 #disply
-    #there is an UFO and you see people floating and being pulled into cargo holds but you managed to find a place with others inside a small compartment but you lost some people you loved
-    #you exit but you don't know what to do exept finish up your schooling
+    #your on your way home from the day of taking and studying collage level science classes with the teachers leaving at the same time as you
+    #The sky went dark and you look up and see that there is an UFO and you see people floating and being pulled into cargo holds
+    #You reliesed that the teachers aren't being pulled into them.
+    #But you managed to find a place with others inside a small compartment.
+    #When you left the space you don't know what happend to some people you know and loved
+    #You have 3 opptions finnish collage with a chemistry, Physics, and engineering degree
+    #Find your known ones
+    #Run in fear from that spaceship
+    #Attack the person that is behind this mass kidnapping
 
 #ask user
-#
+#what do you choose after you finish all of the degrees
+#1 find the people you know and love
+#2 collect men to run
+#3 Collect men to help prepair for war to find the friends that everyone lost and attack the home planet of that ship.
 
-#funtion earth():
-    #items
+
+#choose is True
+#while choose is True:
+#   user input for reasons to use the degrees
+#   if user input is 1
+#       disply "You collect your collage peirs at the end by posting a job that you have for them and they need to work together to go across space as fast as posible knowing that if you fail it will be imposible to find them if they are alive"
+#   if user input is 2
+#       disply "you collect men and women to make a ship to travle as far as you can across the solar system to avoid the alians"
+#   if user input is 3
+#       dysply "you collect everyone that has a major in science and start to work on spaceships that are desined to hold and feed thousands of people with weapons strong enough to damage anything in its path"  
+
+
+
+
+
+    
 
 #disply
 "Search for lost people (hire someone to make a space ship for others to find the lost people and collect ores to make a bigger ship with guns to attack the boss but one will sabotage the weapons)"
