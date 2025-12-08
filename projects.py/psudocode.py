4b9d57d05f23bdddad2a7ce63328153a85eb11ac
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-12-08 09:52:48 -0700

    adsf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

---
+++ projects.py/psudocode.py
@@ -0,0 +1,19 @@
+#BG 1st psudocode to code
+
+rooms = {
+    "room1": "Earth",
+    "room2": "LA",
+    "room3": "UA",
+    "room4": "Quip",
+    "room5": "LASS",
+    "room6": "IPLA",
+    "room7": "BLAT",
+    "room8": "Uit",
+    "room9": "HIPOS"
+}
+user_lives = True
+
+print("You see a spaceship kiddnap hundreds of people")
+print("You team up with a rocket company to free them")
+print("They get you onto the space ship")
+while user_lives == True:
