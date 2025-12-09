4a36e6e88eed48e1964c2ac0af7a35c12ce8df8d
BG2bg2bg2 <gerlachbrett@gmail.com>
2025-12-09 09:50:27 -0700

    asdfadsf
    Signed-off-by: BG2bg2bg2 <gerlachbrett@gmail.com>

--- projects.py/psudocode.py
+++ projects.py/psudocode.py
@@ -1,19 +1,142 @@
 #BG 1st psudocode to code
 
-rooms = {
-    "room1": "Earth",
-    "room2": "LA",
-    "room3": "UA",
-    "room4": "Quip",
-    "room5": "LASS",
-    "room6": "IPLA",
-    "room7": "BLAT",
-    "room8": "Uit",
-    "room9": "HIPOS"
-}
-user_lives = True
+import random as r
+#Data
+#List of rooms available to visit
+#Rooms are described by a dictionary of 9 rooms with the following values
+def rooms(**hi):
+    room = {
+    room1: "Earth"
+    room2: "LA"
+    room3: "Ua"
+    room4: "Quip"
+    room6: "LASS"
+    room7: "IPLA"
+    room8: "BLAT"
+    room9: "Uit"
+    room10: "HIPOS"
+    }
+    return room
+#	Description is text displayed to user
+#	Elements are the artifacts in the room like weapons and shields
+#	Enemies must be fought or run from but if user survives players stats upgrades randomly
 
-print("You see a spaceship kiddnap hundreds of people")
-print("You team up with a rocket company to free them")
-print("They get you onto the space ship")
-while user_lives == True:
+#Elements are a dictionary with the following values which, which change user stats
+user_name = input("enter a name for your character")
+def user_stats():
+#Name,
+    user = {
+    "name": user_name,
+    #health
+    "health": 90,
+    #speed
+    "Speed":2,
+    #defence
+    "defence":5,
+    #stength
+    "stength":10
+    }
+    return user
+
+#Only one element can be used per battle.
+
+enter = input("A")
+if enter == "A":
+    for a in rooms():
+        print(rooms)
+
+#Player and enemies are described by a dictionary named players.
+#Set user’s attributes
+#health = 30
+#Stealth = 0
+#Speed = 2,
+#Defence = 3
+#Strength = 7
+
+#Boss attributes
+
+#	Health 70
+#	Stealth = 3
+#	Speed = 3
+#	Defence = 2
+#	Strength = 30
+
+#Enemy 3 = GLARB
+#	Health = 100
+#	Stealth = 100
+#	Speed = 300
+#	Defence = 200
+#	Strength = 34543
+#Avoid GLARB!!!!!!!!
+
+#Functions:
+#visit_room(room_number)#
+#	Display room description 
+#	If enemy in room do combat
+#	If item in room do item
+
+#do_combat
+    #Function player_attack_roll()
+#    Roll random number between 1 and player strength plus speed
+#    Function player_defend(enemy_damage)
+#    Block = random number between 1 and and player defense minus stealth minus speed plus enemy speed#
+#
+ #   If player or enemy is at 0 end combat with out come of returning health or ending game	
+  #  Use_item
+   #     Describe item
+    #    Ask user to use, discard, or leave item.
+     #   If use then add item attributes to users attributes, remove item from room, place item in users hands.
+      #  Else If discard, subtract item attribute from user attributes and remove item from room and from users hands
+       # Else if leave item  do nothing.
+
+#    Main loop
+ #   While user lives
+  #  Display rooms
+   # If user enters room 1
+    #    display you enter a hidden place on Earth and you see a note and a gun you go to the gun first and are asked if you want to pick it up then you go to read the note
+    #    If user enters room 2
+     #       display you enter a hidden ship called LA (Lost Aliens) and you see a jet pack and a note
+      #      Ask user to pick up body o pack then read note
+       # If user enters room 3
+        #    display you enter a patrol ship called UA (Unified Aliens) and you run into an enemy scout
+         #   Display enemy stats and ask if you want to run or fight
+          #      Do combat if chose to fight
+    #If user enters room 4
+     #           display you enter your outside on this little island with some people you know and some others that you don't recognise but you talk to each one anyways and you get intel of what the place is called (Quip Quick update intel place) and you get intel of what the bosses sats are
+     #   Display boss stats
+      #  Display write them down on a piece of paper
+   # If user enters room 5
+    #Display your in a Lost Alien Space Ship and you see an enemy named GLARB
+    #Display enemy 3 stats
+    #Display run!!!!
+    #Display you died by not running too fast
+    #isplay avoid GLARB at all costs
+   # Display GLARB Great Lost Alien Rebellion to Boss
+    #    If user enters room 6
+     #       Display you enter IPLA Intel Place for Lost Aliens
+      #      Display you see an enemy
+       #     Display enemy stats
+        #        Do combat
+        #If user enters room 7
+         #   Display you enter a place called BLAT and you see a book
+          #  Display you read the book
+           # Display all enemy stats
+           # Display all rooms with enemies
+            #Display rooms to avoid with no loot
+            #Display you leave room never to return
+  #      If user enters room 8
+   #         You enter Uit a place for Unified Intel Trade
+    #            You see a chest with a ball
+     #           Display Ball o shield stats
+      #          Ask to pick up it and use it or discard it
+       # If user enters room 9
+        #    Display you found a key somewhere in the hall and you unlock the door to the room
+         #   Display you see a man on a chair in some random part of the galaxy
+          #  Display You enter my domain isn't my work done well
+           # Display your work is nothing but theory and kidnapping I know why you do it and it is not good
+            #Display I challenge you weather I know or not on how much life you have
+             #   Do combat
+ #   Choose a number for going into a room with that same number to go into
+  #  Call visit room(room number)
+   # If user dead break
+
--- projects.py/tempCodeRunnerFile.py
+++ projects.py/tempCodeRunnerFile.py
@@ -1,39 +1,16 @@
+rooms = {
+    "room1": "Earth",
+    "room2": "LA",
+    "room3": "UA",
+    "room4": "Quip",
+    "room5": "LASS",
+    "room6": "IPLA",
+    "room7": "BLAT",
+    "room8": "Uit",
+    "room9": "HIPOS"
+}
 
-shop = []
-while True:
-    print("\n1 = add item.\n2 = remove item. \n3 = show list.\n4 = clear. \n5 = Quit")
-    num = input("\nchoose a number 1-5 ")
-
-    if num == "1":
-        item = input("\nwhat is the item you want to add? ")
-        shop.append(item)
-        print(f"\n{item} has been added")
-
-    if num == "2":
-        item = input("\nwhat item do you want to remove? ")
-        if item in shop:
-            shop.remove(item)
-            print(f"\n{item} removed")
-        else:
-            print(f"\n{item} not found")
-
-    if num == "3":
-        if shop:
-            for a in enumerate(shop):
-                #print({item}, {a})
-                print("\n not going to work")
-                print(list(shop))
-        else:
-            print("\nNo item on")
-
-    if num == "4":
-        print("\nclear")
-        if shop:
-            shop.clear()
-            print("cleared")
-    if num == "5":
-        print(f"\nQuiting")
-        break
-    else:
-        print("\ntry again")    
-    
+def room(rooom):
+    for itme in rooms:
+        print(rooms)
+    return rooms
